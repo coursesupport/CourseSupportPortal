e847f2fd2a1fe86c13b0d6f65508227c6ff524b6
Seth Childers <chi11008@byui.edu>
2017-03-28 09:51:35 -0600

    Modified Login.php

--- web/midterm/login.php
+++ web/midterm/login.php
@@ -8,41 +8,41 @@ $badLogin = false;
 // First check to see if we have post variables, if not, just
 // continue on as always.
 
-    if (isset($_POST['loginname']) && isset($_POST['loginpass']))
-    {
-        // they have submitted a username and password for us to check
-        $username = $_POST['loginname'];
-        $password = $_POST['loginpass'];
-    
-        // Connect to the DB
-        require("heroku_access.php");
-        $db = get_db();
-        $query = 'SELECT password, id FROM player WHERE username=:username';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':username', $username);
-        $result = $statement->execute();
-        if ($result)
-        {
-            $row = $statement->fetch();
-            $hashedPasswordFromDB = $row['password'];
-        
-            if (password_verify($password, $hashedPasswordFromDB))
-            {
-                // password was correct, put the user on the session, and redirect to home
-                $_SESSION['username'] = $username;
-                $_SESSION['id'] = $row['id'];
-                header("Location: machine.php");
-                die(); // we always include a die after redirects.
-            }
-            else
-            {
-                $badLogin = true;
-            }
-        }
-    }
+if (isset($_POST['loginname']) && isset($_POST['loginpass']))
+{
+	// they have submitted a username and password for us to check
+	$username = $_POST['loginname'];
+	$password = $_POST['loginpass'];
+	
+	// Connect to the DB
+	require("heroku_access.php");
+	$db = get_db();
+	$query = 'SELECT password, id FROM player WHERE username=:username';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':username', $username);
+	$result = $statement->execute();
+	if ($result)
+	{
+		$row = $statement->fetch();
+		$hashedPasswordFromDB = $row['password'];
+		
+		if (password_verify($password, $hashedPasswordFromDB))
+		{
+			// password was correct, put the user on the session, and redirect to home
+			$_SESSION['username'] = $username;
+			$_SESSION['id'] = $row['id'];
+			header("Location: machine.php");
+			die(); // we always include a die after redirects.
+		}
+		else
+		{
+			$badLogin = true;
+		}
+	}
+}
 else
 {
-    $badLogin = true;
+	$badLogin = true;
 }
 
 
@@ -57,76 +57,55 @@ else
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link rel="stylesheet" href="ponder_05.css">
-        <link rel="stylesheet" href="login.css">
-        <link rel="icon" href="welcome_to_the_machine.png">
-        <title>Welcome to Machine!</title>
-    </head>
-    <body>
-        
-        <!-- Header stuff -->
-        
-        <header id="headerround">
-            <div class="dropdown">
-                <button class="dropbtn">Profile</button>
-                <div class="dropdown-content">
-                    <a href="../prove02/home.html">Homepage</a>
-                    <a href="#">About Me</a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn">Assignments</button>
-                <div class="dropdown-content">
-                    <a href="../prove02/assignments.html">Homepage</a>
-                    <a href="../ponder_03/ponder_03.php">Survey</a>
-                    <a href="#">Coming Soon</a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn">DJ/Lighting</button>
-                <div class="dropdown-content">
-                    <a href="http://www.djandx.com/">DJAndX</a>
-                    <a href="#">Island Thunder</a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropbtn">Hammocks/Outdoors</button>
-                <div class="dropdown-content">
-                    <a href="../ponder_02/assign03/assign03.html">Rexburg Hammocks</a>
-                </div>
-            </div>
-       </header>
-        
-        <br/>
-        
-        
-        <!-- Error is displayed if login does not work -->
-        
-        <?php
-        /*if ($badLogin)
-        {
-            echo "Incorrect username or password!<br /><br />\n";
-        }*/
-        ?>
-
-        <!-- This is the login credentials page to login. Option for creating a new account is also available. -->
-        
-        <div id="standard2">
-            <div id="center">
-                <p>Welcome to Machine! Your one stop for increased productivity!</p>
-                <form action="login.php" method="post">
-                    <p>Username:
-                        <input type="text" name="loginname">
-                        <br/>
-                        Password:
-                        <input type="password" name="loginpass">
-                    </p>
-                    <input type="submit">
-                </form>
-                <hr/>
-                <p>Don't have an account? Create one <a href="account_setup.php">here</a>.</p>
-            </div>
-        </div>
-    </body>
+	<head>
+		<link rel="stylesheet" href="ponder_05.css">
+		<link rel="stylesheet" href="login.css">
+		<link rel="icon" href="welcome_to_the_machine.png">
+		<title>Welcome to Machine!</title>
+	</head>
+	<body>
+		
+		<!-- Navbar -->
+		<div class="w3-top">
+			<div class="w3-bar w3-black w3-card-2">
+				<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+				<a href="#" class="w3-bar-item w3-button w3-padding-large">Course Support</a>
+				<a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Home</a>
+				<a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
+				<a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><em>Username</em></a>
+				<a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+			</div>
+		</div>
+		
+		<!-- Login Section -->
+		<div class="w3-content" style="max-width:2000px;margin-top:46px">
+			
+			<!-- The Band Section -->
+			<div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+				<h2 class="w3-wide">Course Support</h2>
+				<p class="w3-opacity"><i>Login here</i></p>
+				
+				
+				
+				<!-- The Tour Section -->
+				<div class="w3-black" id="tour">
+					<div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+						<h2 class="w3-wide w3-center">Sign in here</h2>
+						<p class="w3-opacity w3-center"><i>If you want to gain access that is...</i></p>
+						<form action="login.php" method="post">
+							<ul class="w3-ul w3-border w3-white w3-text-grey">
+								<li class="w3-padding">Username <input type="text" size="auto" name="loginname" required/></li>
+								<li class="w3-padding">Password <input type="password" size="auto" name="loginpass" required /></li>
+							</ul>
+							<br/>
+							<input type="submit" />
+						</form>
+						<p>Don't have an account? Create one <a href="account_setup.php">here</a>!</p>
+					</div>
+				</div>
+			</div>
+			<!-- End Page Content -->
+		</div>
+		
+	</body>
 </html>
