002e5b33c8353ab404296ad6efae85fe2522103e
Seth Childers <chi11008@byui.edu>
2017-03-28 09:53:28 -0600

    Modified Login.php

--- web/midterm/login.php
+++ web/midterm/login.php
@@ -58,10 +58,18 @@ else
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" href="ponder_05.css">
-		<link rel="stylesheet" href="login.css">
-		<link rel="icon" href="welcome_to_the_machine.png">
+		<title>Course Support | Login</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<title>Welcome to Machine!</title>
+		
+		<style>
+			body {font-family: "Lato", sans-serif}
+			.mySlides {display: none}
+		</style>
 	</head>
 	<body>
 		
