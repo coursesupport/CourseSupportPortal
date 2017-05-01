bce8c21bbb51dd79ee4312e21a513f91a82454f1
Seth Childers <chi11008@byui.edu>
2017-05-01 15:25:09 -0700

    Put the footer into each file

--- web/php/account_setup.php
+++ web/php/account_setup.php
@@ -1,77 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-		<title>Course Support | Account Setup</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		
-		<!-- favicon -->
-		<link rel="icon" type="image/png" href="/images/favicon.ico" />
-       
-        <script src="../javascript.js"></script>
-       
-    </head>
-    <body>
-    
-        <!-- Tabs across the top of the page -->
-        
-        <!-- Navbar -->
-        <div class="w3-top">
-            <div class="w3-bar w3-black w3-card-2">
-                <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-                <a href="#" class="w3-bar-item w3-button w3-padding-large">Profile</a>
-                <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
-                <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Logout</a>
-                <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><em>Username</em></a>
-                <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+   <head>
+      <title>Course Support | Account Setup</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+      <!-- favicon -->
+      <link rel="icon" type="image/png" href="/images/favicon.ico" />
+      
+      <script src="../javascript.js"></script>
+      
+   </head>
+   <body>
+      
+      <!-- Tabs across the top of the page -->
+      
+      <!-- Navbar -->
+      <div class="w3-top">
+         <div class="w3-bar w3-black w3-card-2">
+            <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+            <a href="#" class="w3-bar-item w3-button w3-padding-large">Profile</a>
+            <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
+            <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Logout</a>
+            <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><em>Username</em></a>
+            <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+         </div>
+      </div>
+      
+      <!-- The Band Section -->
+      <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+         <h2 class="w3-wide">Welcome to the Team!</h2>
+         <p class="w3-opacity"><i>Start by creating an account</i></p>
+         
+         
+         
+         <!-- The Tour Section -->
+         <div class="w3-black" id="tour">
+            <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+               <h2 class="w3-wide w3-center">Suit Up Here</h2>
+               <p class="w3-opacity w3-center"><i>This is your first step to be coming Legen... wait for it... dary</i></p>
+               <form action="account_creation.php" method="post">
+                  <ul class="w3-ul w3-border w3-white w3-text-grey">
+                     <li class="w3-padding">First Name <input type="text" size="auto" name="new_firstname" required /></li>
+                     <li class="w3-padding">Last Name <input type="text" size="auto" name="new_lastname" required /></li>
+                     <li class="w3-padding">Username <input type="text" size="auto" name="new_user" required/></li>
+                     <li class="w3-padding">Password <input type="password" size="auto" name="new_pass" required /></li>
+                     
+                     <!-- The re-entry of password can be validated with javascript easier than with php as well as throw an alert if it is not correct. -->
+                     <li class="w3-padding">Re-enter Password <input type="password" size="auto" name="new_pass2" onkeydown="passwordCheck();" required /></li>
+                  </ul>
+                  <br/>
+                  <input type="submit" value="Submit"/>
+               </form>
             </div>
-        </div>
-    
-        <!-- The Band Section -->
-        <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
-            <h2 class="w3-wide">Welcome to the Team!</h2>
-            <p class="w3-opacity"><i>Start by creating an account</i></p>
-                
-                
-                
-            <!-- The Tour Section -->
-            <div class="w3-black" id="tour">
-                <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
-                    <h2 class="w3-wide w3-center">Suit Up Here</h2>
-                    <p class="w3-opacity w3-center"><i>This is your first step to be coming Legen... wait for it... dary</i></p>
-                    <form action="account_creation.php" method="post">
-                        <ul class="w3-ul w3-border w3-white w3-text-grey">
-                            <li class="w3-padding">First Name <input type="text" size="auto" name="new_firstname" required /></li>
-                            <li class="w3-padding">Last Name <input type="text" size="auto" name="new_lastname" required /></li>
-                            <li class="w3-padding">Username <input type="text" size="auto" name="new_user" required/></li>
-                            <li class="w3-padding">Password <input type="password" size="auto" name="new_pass" required /></li>
-                            
-                            <!-- The re-entry of password can be validated with javascript easier than with php as well as throw an 
-                                 alert if it is not correct. -->
-                            <li class="w3-padding">Re-enter Password <input type="password" size="auto" name="new_pass2" onkeydown="passwordCheck();" required /></li>
-                        </ul>
-                        <br/>
-                        <input type="submit" value="Submit"/>
-                    </form>
-                </div>
-            </div>
-        </div>
-        
-        
-        
-        <!-- Footer -->
-        <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
-            <i class="fa fa-facebook-official w3-hover-text-indigo"></i>
-            <i class="fa fa-instagram w3-hover-text-purple"></i>
-            <i class="fa fa-snapchat w3-hover-text-yellow"></i>
-            <i class="fa fa-pinterest-p w3-hover-text-red"></i>
-            <i class="fa fa-twitter w3-hover-text-light-blue"></i>
-            <i class="fa fa-linkedin w3-hover-text-indigo"></i>
-            <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
-        </footer>
-    </body>
+         </div>
+      </div>
+      
+      
+      
+      <!-- Footer -->
+      <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+         <div class="w3-container icons">
+            <div class="brightspace" tooltip="brightspace"></div>
+            <div class="pathway"></div>
+            <div class="equella"></div>
+            <div class="outlook"></div>
+            <div class="teamdynamix"></div>
+            <div class="workplace"></div>
+            <div class="coursecouncil"></div>
+            <div class="workday"></div>
+            <div class="portfolio"></div>
+         </div>
+         <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a><br>Designed by Seth Childers and Island Thunder</p>
+      </footer>
+      
+   </body>
 </html>
-
--- web/php/login.php
+++ web/php/login.php
@@ -83,59 +83,75 @@ else
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Course Support | Login</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
-		<link rel="stylesheet" href="../style.css" />
-
-		<!-- Favicon -->
-		<link rel="icon" type="image/png" href="/images/favicon.ico" />
-	</head>
-	<body>
-		
-		<!-- Navbar -->
-		<div class="w3-top">
-			<div class="w3-bar w3-black w3-card-2">
-				<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-				<a href="#" class="w3-bar-item w3-button w3-padding-large">Course Support</a>
-				<a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Home</a>
-				<a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
-			</div>
-		</div>
-		
-		<!-- Login Section -->
-		<div class="w3-content" style="max-width:2000px;margin-top:46px">
-			
-			<!-- The Band Section -->
-			<div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
-				<h2 class="w3-wide">Course Support</h2>
-				<p class="w3-opacity"><i>Login here</i></p>
-				
-				
-				
-				<!-- The Tour Section -->
-				<div class="w3-black" id="tour">
-					<div class="w3-container w3-content w3-padding-64" style="max-width:800px">
-						<h2 class="w3-wide w3-center">Sign in here</h2>
-						<p class="w3-opacity w3-center"><i>If you want to gain access that is...</i></p>
-						<form action="login.php" method="post">
-							<ul class="w3-ul w3-border w3-white w3-text-grey">
-								<li class="w3-padding">Username <input type="text" size="auto" name="loginname" required/></li>
-								<li class="w3-padding">Password&nbsp; <input type="password" size="auto" name="loginpass" required /></li>
-							</ul>
-							<br/>
-							<input type="submit" />
-						</form>
-						<p>Don't have an account? Create one <a href="account_setup.php">here</a>!</p>
-					</div>
-				</div>
-			</div>
-			<!-- End Page Content -->
-		</div>
-		
-	</body>
+   <head>
+      <title>Course Support | Login</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+      <link rel="stylesheet" href="../style.css" />
+      
+      <!-- Favicon -->
+      <link rel="icon" type="image/png" href="/images/favicon.ico" />
+   </head>
+   <body>
+      
+      <!-- Navbar -->
+      <div class="w3-top">
+         <div class="w3-bar w3-black w3-card-2">
+            <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+            <a href="#" class="w3-bar-item w3-button w3-padding-large">Course Support</a>
+            <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Home</a>
+            <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
+         </div>
+      </div>
+      
+      <!-- Login Section -->
+      <div class="w3-content" style="max-width:2000px;margin-top:46px">
+         
+         <!-- The Band Section -->
+         <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+            <h2 class="w3-wide">Course Support Dashboard</h2>
+            <p class="w3-opacity"><i>Login here</i></p>
+            
+            
+            
+            <!-- The Tour Section -->
+            <div class="w3-black" id="tour">
+               <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+                  <h2 class="w3-wide w3-center">Sign in here</h2>
+                  <p class="w3-opacity w3-center"><i>If you want to gain access that is...</i></p>
+                  <form action="login.php" method="post">
+                     <ul class="w3-ul w3-border w3-white w3-text-grey">
+                        <li class="w3-padding">Username <input type="text" size="auto" name="loginname" required/></li>
+                        <li class="w3-padding">Password&nbsp; <input type="password" size="auto" name="loginpass" required /></li>
+                     </ul>
+                     <br/>
+                     <input type="submit" />
+                  </form>
+                  <p>Don't have an account? Create one <a href="account_setup.php">here</a>!</p>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- End Page Content -->
+      
+      <!-- Footer -->
+      <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+         <div class="w3-container icons">
+            <div class="brightspace" tooltip="brightspace"></div>
+            <div class="pathway"></div>
+            <div class="equella"></div>
+            <div class="outlook"></div>
+            <div class="teamdynamix"></div>
+            <div class="workplace"></div>
+            <div class="coursecouncil"></div>
+            <div class="workday"></div>
+            <div class="portfolio"></div>
+         </div>
+         <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a><br>Designed by Seth Childers and Island Thunder</p>
+      </footer>
+      
+   </body>
 </html>
